67c577f0837cb9d6bd99b53171bca465916d4a65
Mike Shaw <michaelsshaw@hotmail.com>
2025-08-27 13:58:49 -0400

    Created database and added database role for postgres

--- src/server/inventory.test.ts
+++ src/server/inventory.test.ts
@@ -2,8 +2,8 @@ import request from 'supertest';
 import app from './index';
 
 describe('Inventory API', () => {
-  it('should return 200 for GET /', async () => {
-    const res = await request(app).get('/');
+  it('should return 200 for GET /api', async () => {
+    const res = await request(app).get('/api');
     expect(res.statusCode).toBe(200);
   });
 });
